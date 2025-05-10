dc0ae662ece788cb9466da94c6ff6b2a5ec22ba9
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-05-10 22:17:03 +0300

    basket Controller

--- infrastructure/presentation/Controllers/BasketController.cs
+++ infrastructure/presentation/Controllers/BasketController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ServicesAbstraction;
+using Shared.DataTransferObjects.Basket;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,12 +15,34 @@ namespace Presentation.Controllers
     {
 
         //1)Get User Basket
+        [HttpGet]
+        public async Task<ActionResult<BasketDto>> Get(string Id)
+        {
+
+            var basket = await _serviceManager.BasketService.GetAsync(Id);
+            return Ok(basket);
+        }
 
         //2)Update User Basket:
         //2.1) Create Basker
         //2.2) Add Item to Basket
         //2.3) Remove Item from Basket
         //2.4) Update Basket Item's Quantity +/-
+
+        [HttpPost]
+        public async Task<ActionResult<BasketDto>> Update(BasketDto basketDto)
+        {
+            var basket = await _serviceManager.BasketService.UpdateAsync(basketDto);
+            return Ok(basket);
+        }
+
+        [HttpDelete]
+
+        public async Task<ActionResult<bool>> Delete(string Id)
+        {
+            await _serviceManager.BasketService.DeleteAsync(Id);
+            return NoContent();
+        }
 
         //3)Delete User Basket : After Checkout => Empty Basket
 
