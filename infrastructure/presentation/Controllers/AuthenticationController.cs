4c557ee481d457b501e7b66dd180b0469f43eb00
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-05-22 17:47:39 +0300

    Some Actions in Auth controller

--- infrastructure/presentation/Controllers/AuthenticationController.cs
+++ infrastructure/presentation/Controllers/AuthenticationController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using ServicesAbstraction;
 using Shared.Authentication;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,6 +26,40 @@ namespace Presentation.Controllers
         public async Task<ActionResult<UserResponse>> Register(RegisterRequest request)
         {
             return Ok(await _serviceManager.AuthenticationService.RegisterAsync(request));
+        }
+
+
+        [HttpGet("emailexists")]
+        public async Task<ActionResult<bool>> CheckEmail(string email)
+        {
+            return Ok(await _serviceManager.AuthenticationService.CheckEmailAsync(email));
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<UserResponse>> GetUser()
+        {
+            var mail = User.FindFirstValue(ClaimTypes.Email);
+
+            return Ok(await _serviceManager.AuthenticationService.GetUserByEmail(mail));
+        }
+
+        [Authorize]
+        [HttpGet("address")]
+        public async Task<ActionResult<AddressDto>> GetAddress()
+        {
+            var mail = User.FindFirstValue(ClaimTypes.Email);
+
+            return Ok(await _serviceManager.AuthenticationService.GetUserAddress(mail));
+        }
+
+
+        [Authorize]
+        [HttpPut("Address")]
+        public async Task<ActionResult<AddressDto>> UpdateAddress(AddressDto address)
+        {
+            var mail = User.FindFirstValue(ClaimTypes.Email);
+            return Ok(await _serviceManager.AuthenticationService.UpdateUserAddressAsync(address, mail));
         }
 
     }
